65a9c0258da2bc2268ed76949dadbaefb4ea3baa
MrArtik4762 <oartem476@gmail.com>
2025-08-04 20:00:51 +0300

    починка

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+// client/.eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
+    'no-unused-vars': ['warn', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-template': 'error',
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'jsx-a11y/label-has-associated-control': ['error', {
+      labelComponents: ['label'],
+      labelAttributes: ['htmlFor'],
+      controlComponents: ['input', 'select', 'textarea'],
+      depth: 3
+    }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'build/',
+    'node_modules/',
+    'public/',
+    'dist/',
+    '*.config.js'
+  ],
+};
